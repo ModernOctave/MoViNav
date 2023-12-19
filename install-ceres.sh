8d2ee64b9d0be3d321504a9fab8c4cc3ac7d13c1
Om Patil <84833795+ModernOctave@users.noreply.github.com>
2023-12-19 11:14:19 +0530

    Update install-ceres.sh

--- install-ceres.sh
+++ install-ceres.sh
@@ -2,8 +2,7 @@
 
 sudo apt-get update &&\
 sudo apt-get install -y wget &&\
-wget -nc http://ceres-solver.org/ceres-solver-2.1.0.tar.gz &&\
-tar zxf ceres-solver-2.1.0.tar.gz &&\
+git clone -b 2.1.0 https://ceres-solver.googlesource.com/ceres-solver &&\
 sudo apt-get install -y cmake &&\
 sudo apt-get install -y libgoogle-glog-dev libgflags-dev &&\
 sudo apt-get install -y libatlas-base-dev &&\
@@ -11,7 +10,7 @@ sudo apt-get install -y libeigen3-dev &&\
 sudo apt-get install -y libsuitesparse-dev &&\
 mkdir ceres-bin &&\
 cd ceres-bin &&\
-cmake ../ceres-solver-2.1.0 &&\
+cmake ../ceres-solver &&\
 make -j3 &&\
 make test &&\
 make install
